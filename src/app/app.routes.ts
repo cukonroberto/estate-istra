4cd7ca9e8087acc7c4678290528ffd256c983df5
cukonroberto <cukonroberto@gmail.com>
2026-01-15 13:08:46 +0100

    Dodani kosturi zasebnih stranica sa namještenim rutama.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Properties } from './pages/properties/properties';
+import { HowWeWork } from './pages/how-we-work/how-we-work';
+import { PropertyDetail } from './pages/property-detail/property-detail';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'nekretnine', component: Properties },
+  { path: 'nekretnina/:id', component: PropertyDetail },
+  { path: 'kako-radimo', component: HowWeWork },
+  { path: 'kontakt', component: Contact },
+  { path: '**', redirectTo: '' },
+];
